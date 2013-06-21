1dfa0d543e2c143503d4401b80ad5f25f31291f9
gunther <info@g-raph.be>
2013-06-21 17:23:56 +0200

    Menu's and titles.

--- sites/all/themes/dcl/js/custom.js
+++ sites/all/themes/dcl/js/custom.js
@@ -3,13 +3,13 @@
     attach: function(context, settings) {
 
       // secondary menu
-      var secMenu = $('#header-top-inner #navigation');
+      var secMenu = $('#header-top-inner #navigation ul');
       secMenu.hide();
       $('<div id="navbutton"></div>').insertBefore(secMenu);
       $('#navbutton').click(function(){
       	secMenu.slideToggle('fast');
       	$(this).toggleClass('open');
-      	
+
       });
 
     }
