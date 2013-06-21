7200b2f9873d192a8afdd6c6c735fd9320417c99
gunther <info@g-raph.be>
2013-06-21 16:19:43 +0200

    Initial theming commit: regions defined, header styling.

---
+++ sites/all/themes/dcl/js/custom.js
@@ -0,0 +1,17 @@
+(function($) {
+  Drupal.behaviors.theming = {
+    attach: function(context, settings) {
+
+      // secondary menu
+      var secMenu = $('#header-top-inner #navigation');
+      secMenu.hide();
+      $('<div id="navbutton"></div>').insertBefore(secMenu);
+      $('#navbutton').click(function(){
+      	secMenu.slideToggle('fast');
+      	$(this).toggleClass('open');
+      	
+      });
+
+    }
+  };
+})(jQuery);
