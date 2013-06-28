35c89c33c2ab176772c966a2db275f5527be1a8e
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2013-06-28 12:49:26 +0100

    Exclude template.php

--- sites/leuven2013/themes/dcl/template.php
+++ sites/leuven2013/themes/dcl/template.php
@@ -7,7 +7,7 @@ function dcl_preprocess_block(&$vars) {
   $block_id = $vars['block']->module . '-' . $vars['block']->delta;
   $classes = &$vars['classes_array'];
 
-  if ($block_id == 'leuven2013_general-banner_to_frontpage' || $block_id == 'views-leuven2013_sponsors-block_1') {
+  if (!drupal_is_front_page() && ($block_id == 'leuven2013_general-banner_to_frontpage' || $block_id == 'views-leuven2013_sponsors-block_1')) {
     $classes[] = 'col6';
   }
 }
