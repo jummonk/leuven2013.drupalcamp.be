d0412790c68804262797280d985c9909e9b7ec15
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2013-07-08 14:27:20 +0200

    [#27] Added colors to taxonomy term rendering on program page

--- sites/leuven2013/themes/dcl/template.php
+++ sites/leuven2013/themes/dcl/template.php
@@ -14,7 +14,35 @@ function dcl_preprocess_block(&$vars) {
 
 function dcl_preprocess_field(&$variables, $hook) {
   $element = $variables['element'];
-  if ($element['#name'] == 'field_session_track') {
-    $variables['classes_array'][] = 'BLUE';
+  if ($element['#field_name'] == 'field_session_track') {
+    foreach ($variables['items'] as $delta => $item) {
+      $colors = field_get_items('taxonomy_term', $item['#options']['entity'], 'field_track_color_code');
+      if (!empty($colors) && !empty($colors[0]['rgb'])) {
+        $rgb = $colors[0]['rgb'];
+        // $rgb = str_replace('#', '', $rgb);
+        $variables['item_attributes_array'][$delta]['style'] = 'background: ' . $rgb;
+      }
+    }
   }
+}
+
+function dcl_field($variables) {
+  $output = '';
+ 
+  // Render the label, if it's not hidden.
+  if (!$variables['label_hidden']) {
+    $output .= '<div ' . $variables['title_attributes'] . '>' . $variables['label'] . ':&nbsp;</div>';
+  }
+ 
+  // Render the items.
+  $output .= '<div ' . $variables['content_attributes'] . '>';
+  foreach ($variables['items'] as $delta => $item) {
+    $output .= '<div ' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</div>';
+  }
+  $output .= '</div>';
+ 
+  // Render the top-level DIV.
+  $output = '<div class="' . $variables['classes'] . '"' . $variables['attributes'] . '>' . $output . '</div>';
+ 
+  return $output;
 }
